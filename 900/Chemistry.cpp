c85e77a5eccd549de3c3e3da16baedd1e42d824c
Aryaman30 <111583885+Aryaman3007@users.noreply.github.com>
2024-01-06 23:33:45 +0530

    day3

---
+++ 900/Chemistry.cpp
@@ -0,0 +1,33 @@
+/*  21UEC005_Aryaman_Singh   */
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <climits>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <unordered_set>
+#define loopf for(ll i=0;i<n;i++)
+#define A ios_base::sync_with_stdio(false);
+#define R cin.tie(NULL);
+#define Y cout.tie(NULL);
+#define tcs ll T;cin>>T;while(T--)
+#define ll long long int
+#define vecas vector<ll> v; for(ll i=0;i<n;i++) {ll x; cin>>x; v.push_back(x);}
+using namespace std;
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+     freopen("input.txt", "r", stdin);
+     freopen("output.txt", "w", stdout);
+    #endif
+    A R Y
+    tcs
+    {
+        
+    }
+    return 0;
+}
