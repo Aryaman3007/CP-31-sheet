9389d03995e4cefbc770f63e9cb516bd16841ccc
Aryaman30 <111583885+Aryaman3007@users.noreply.github.com>
2024-01-06 23:34:49 +0530

    day3

--- 900/Forked.cpp
+++ 900/Forked.cpp
@@ -27,7 +27,43 @@ int main()
     A R Y
     tcs
     {
-                
+        //Make a set of all the 8 possible neighbours of king and queen and count the number of common locations 
+        int a,b;
+        cin>>a>>b;
+        int xk,yk;
+        cin>>xk>>yk;
+        int xq,yq;
+        cin>>xq>>yq;
+        set<pair<int,int>> s1;                        
+        set<pair<int,int>> s2;
+        vector<int> adj = {-1,1};
+        for(int i=0;i<2;i++){
+            for(int j=0;j<2;j++){
+                int x1=xk+adj[i]*a;
+                int y1=yk+adj[j]*b;
+                int x2=xq+adj[i]*a;
+                int y2=yq+adj[j]*b;
+                s1.insert({x1,y1});
+                s2.insert({x2,y2});
+            }
+        } 
+        for(int i=0;i<2;i++){
+            for(int j=0;j<2;j++){
+                int x1=xk+adj[i]*b;
+                int y1=yk+adj[j]*a;
+                int x2=xq+adj[i]*b;
+                int y2=yq+adj[j]*a;
+                s1.insert({x1,y1});
+                s2.insert({x2,y2});
+            }
+        } 
+        int cnt=0;
+        for(auto it:s1){
+            if(s2.find({it.first,it.second}) != s2.end()){
+                cnt++;
+            }
+        } 
+        cout<<cnt<<endl;                      
     }
     return 0;
 }
