341c89bbddba808d003004f48db62833275bb682
Aryaman30 <111583885+Aryaman3007@users.noreply.github.com>
2024-01-11 00:28:12 +0530

    day4

--- 900/Chemistry.cpp
+++ 900/Chemistry.cpp
@@ -1,14 +1,5 @@
 /*  21UEC005_Aryaman_Singh   */
-#include <iostream>
-#include <algorithm>
-#include <cmath>
-#include <climits>
-#include <vector>
-#include <map>
-#include <set>
-#include <queue>
-#include <stack>
-#include <unordered_set>
+#include <bits/stdc++.h>
 #define loopf for(ll i=0;i<n;i++)
 #define A ios_base::sync_with_stdio(false);
 #define R cin.tie(NULL);
@@ -27,6 +18,23 @@ int main()
     A R Y
     tcs
     {
+        //check if there are more than 1 odd character occurences after removing k elements, if there are then print "NO" else "YES"
+        int n,k;
+        cin>>n>>k;
+        string s;
+        cin>>s;
+        unordered_map<char,int> mp;
+        for(int i=0;i<s.size();i++){
+            mp[s[i]]++;
+        }
+        int cnt=0;
+        for(auto it:mp){
+            if((it.second%2) !=0){
+                cnt++;
+            }
+        }
+        if(k<cnt-1) cout<<"NO\n";
+        else cout<<"YES\n";
         
     }
     return 0;
